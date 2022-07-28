4f4be20804ed7238685423bc9dbd59b7a67a86d1
kevin <kevin.brunclaire@le-campus-numerique.fr>
2022-07-28 11:33:55 +0200

    ajout item.php

--- item.php
+++ item.php
@@ -1,1 +1,26 @@
 <?php
+$name = 'TV';
+$Price = '999 euros';
+$URL = 'Assets/tv.jpg';
+?>
+
+
+<!doctype html>
+
+<html lang="fr">
+<head>
+    <title>test premier site</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+<h1>Welcome</h1>
+
+<h2>Only one article : <?= $name ?> </h2>
+
+<h2>Price : <?= $Price ?></h2>
+
+<div> <img src="<?= $URL?>" alt="photo tv"
+
+</div>
+</body>
+</html>
